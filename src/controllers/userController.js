7d4aba52759205e19c3bd22bbb09abb5fe1b1a0b
RitikaPathania03 <ritikapathania520@gmail.com>
2022-06-16 07:30:09 +0530

    making changes to save in my branch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,11 +5,11 @@ const createUser = async function (req, res) {
   //You can name the req, res objects anything.
   //but the first parameter is always the request 
   //the second parameter is always the response
-//   let data = req.body;
-//   let savedData = await userModel.create(data);
-//   console.log(req.newAtribute);
-//   res.send({ msg: savedData });
-// };
+  let data = req.body;
+  let savedData = await userModel.create(data);
+  console.log(req.newAtribute);
+  res.send({ msg: savedData });
+};
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
